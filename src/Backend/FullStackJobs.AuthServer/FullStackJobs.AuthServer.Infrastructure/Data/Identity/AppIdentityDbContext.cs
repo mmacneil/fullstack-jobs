fe285215b2ff0d0e60514746942d04cc3aa6462a
mmacneil <markmacneil@gmail.com>
2019-11-23 21:38:11 -0400

    AppIddentityDbContext

---
+++ src/Backend/FullStackJobs.AuthServer/FullStackJobs.AuthServer.Infrastructure/Data/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackJobs.AuthServer.Infrastructure.Data.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);            
+        }
+    }
+}
