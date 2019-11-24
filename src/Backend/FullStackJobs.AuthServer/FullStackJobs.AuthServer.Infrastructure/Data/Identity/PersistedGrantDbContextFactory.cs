a844203c5a1ee7f625f4d1226f9ed5fd3154434b
mmacneil <markmacneil@gmail.com>
2019-11-23 23:14:33 -0400

    Post wip.

---
+++ src/Backend/FullStackJobs.AuthServer/FullStackJobs.AuthServer.Infrastructure/Data/Identity/PersistedGrantDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FullStackJobs.AuthServer.Infrastructure.Data.Identity
+{
+    public class PersistedGrantDbContextFactory : DesignTimeDbContextFactoryBase<PersistedGrantDbContext>
+    {
+        protected override PersistedGrantDbContext CreateNewInstance(DbContextOptions<PersistedGrantDbContext> options)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext>();
+            optionsBuilder.UseSqlServer(ConnectionString, sql => sql.MigrationsAssembly(this.GetType().GetTypeInfo().Assembly.GetName().Name));
+            return new PersistedGrantDbContext(optionsBuilder.Options, new OperationalStoreOptions());
+        }
+    }
+}
