f0df94478fa4c9416808b1ee709de5f1ef60264f
mmacneil <markmacneil@gmail.com>
2019-12-22 21:39:57 -0400

    AccountModule wip.

---
+++ src/Frontend/Spa/src/app/account/account.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup/signup.component';
+
+
+
+@NgModule({
+  declarations: [SignupComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AccountModule { }
