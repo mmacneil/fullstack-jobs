625e4302a2f420e0f603d443b6d26e7017d57b00
mmacneil <markmacneil@gmail.com>
2019-12-29 14:02:59 -0400

    Signup/login wip.

--- src/Frontend/Spa/src/app/shell/header/header.component.ts
+++ src/Frontend/Spa/src/app/shell/header/header.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../../core/authentication/auth.service';
 import { Subscription } from 'rxjs';
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   name: string;
   isAuthenticated: boolean;
   subscription:Subscription;
 
-  constructor() { } 
+  constructor(private authService:AuthService) { }
+
+  ngOnInit() {
+    this.subscription = this.authService.authNavStatus$.subscribe(status => this.isAuthenticated = status);
+    this.name = this.authService.name;    
+  } 
+  
+  async signout() {
+    await this.authService.signout();
+  }
+
+  ngOnDestroy() {
+    // prevent memory leak when component is destroyed
+    this.subscription.unsubscribe();
+  }
 }
