df52b1f23aa126c287208583c5a17bb3ff44d92b
mmacneil <markmacneil@gmail.com>
2019-12-31 15:19:03 -0400

    Login component wip.

--- src/Frontend/Spa/src/app/account/account-routing.module.ts
+++ src/Frontend/Spa/src/app/account/account-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 import { Shell } from './../shell/shell.service';
 
 const routes: Routes = [
 Shell.childRoutes([
-    { path: 'signup', component: SignupComponent }   
+    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent }     
   ])
 ];
 
--- src/Frontend/Spa/src/app/account/login/login.component.ts
+++ src/Frontend/Spa/src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/authentication/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+      await this.authService.login();
   }
-
 }
