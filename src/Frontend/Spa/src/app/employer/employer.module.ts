199c87c32deff44bbcf5ba3b9d9c177e36e7a09d
mmacneil <markmacneil@gmail.com>
2020-03-01 20:50:08 -0400

    Employer module wip.

--- src/Frontend/Spa/src/app/employer/employer.module.ts
+++ src/Frontend/Spa/src/app/employer/employer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JobsComponent } from './jobs/jobs.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [JobsComponent],
   imports: [
     CommonModule
   ]
